5ffaab8bd8edde6cfb6e0649fdc38a746deee623
Nikita Korevo <48645737+NikitaKorevo@users.noreply.github.com>
2021-07-01 11:13:40 +0300

    feat: reverse number

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+  let reverseNumber = String(n).split('');
+
+  for (let i = 0; i < reverseNumber.length; i++) {
+    if (reverseNumber[i] === '-') reverseNumber.splice(i, 1);
+  }
+
+  reverseNumber = reverseNumber.reverse().join('');
+  return reverseNumber;
 }
